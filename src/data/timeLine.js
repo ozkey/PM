e025689f7475b51f8d9f1c7de23cacf6de9591cb
ozkey <ozkey@ozkey.com>
2019-05-16 20:30:30 +1000

    playing with bios

--- src/data/timeLine.js
+++ src/data/timeLine.js
@@ -11,7 +11,7 @@ export const timeLine = [
     start: "24/September/1903",
     end: "27/April/1904",
     image:
-        "https://upload.wikimedia.org/wikipedia/commons/e/e7/Alfred_Deakin_crop.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/e/e7/Alfred_Deakin_crop.jpg",
 
     party: "Protectionist"
   },
@@ -20,7 +20,7 @@ export const timeLine = [
     start: "27/April/1904",
     end: "18/August/1904",
     image:
-        "https://upload.wikimedia.org/wikipedia/commons/2/28/ChrisWatsonBW_crop.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/2/28/ChrisWatsonBW_crop.jpg",
     party: "Labour"
   },
 
@@ -29,7 +29,7 @@ export const timeLine = [
     start: "18/August/1904",
     end: "5/July/1905",
     image:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/George_Reid_crop.jpg/800px-George_Reid_crop.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/George_Reid_crop.jpg/800px-George_Reid_crop.jpg",
     party: "Free Trade"
   },
 
@@ -38,7 +38,7 @@ export const timeLine = [
     start: "5/July/1905",
     end: "13/November/1908",
     image:
-        "https://upload.wikimedia.org/wikipedia/commons/b/ba/AlfredDeakin.jpeg",
+      "https://upload.wikimedia.org/wikipedia/commons/b/ba/AlfredDeakin.jpeg",
     party: "Protectionist"
   },
   {
@@ -46,16 +46,15 @@ export const timeLine = [
     start: "13/November/1908",
     end: "2/June/1909",
     image:
-        "https://upload.wikimedia.org/wikipedia/commons/b/be/Andrew_Fisher_1908.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/b/be/Andrew_Fisher_1908.jpg",
     party: "Labour"
-  }
-  ,
+  },
   {
     name: "Alfred Deakin",
     start: "2/June/1909",
     end: "29/April/1910",
     image:
-        "https://upload.wikimedia.org/wikipedia/commons/b/ba/AlfredDeakin.jpeg",
+      "https://upload.wikimedia.org/wikipedia/commons/b/ba/AlfredDeakin.jpeg",
     party: "Commonwealth Liberal"
   },
   {
@@ -63,83 +62,259 @@ export const timeLine = [
     start: "29/April/1910",
     end: "24/June/1913",
     image:
-        "https://upload.wikimedia.org/wikipedia/commons/b/be/Andrew_Fisher_1908.jpg",
-    party: "Labour"
-  }
-
-
-
-
-
-  ,
-
-
-
-  {
-    name: "Sir Edmund Barton",
-    start: "1/January/1901",
-    end: "24/September/1903",
-    image: "https://cdn.britannica.com/s:700x450/07/155007-004-E4208613.jpg",
-    party: "Protectionist"
-  },
-  {
-    name: "Alfred Deakin",
-    start: "24/September/1903",
-    end: "27/April/1904",
-    image:
-        "https://upload.wikimedia.org/wikipedia/commons/e/e7/Alfred_Deakin_crop.jpg",
-
-    party: "Protectionist"
-  },
-  {
-    name: "Chris Watson",
-    start: "27/April/1904",
-    end: "18/August/1904",
-    image:
-        "https://upload.wikimedia.org/wikipedia/commons/2/28/ChrisWatsonBW_crop.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/b/be/Andrew_Fisher_1908.jpg",
     party: "Labour"
   },
 
   {
-    name: "George Reid",
-    start: "18/August/1904",
-    end: "5/July/1905",
+    name: "Joseph Cook",
+    start: "24/June/1913",
+    end: "17/September/1914",
     image:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/George_Reid_crop.jpg/800px-George_Reid_crop.jpg",
-    party: "Free Trade"
-  },
-
-  {
-    name: "Alfred Deakin",
-    start: "5/July/1905",
-    end: "13/November/1908",
-    image:
-        "https://upload.wikimedia.org/wikipedia/commons/b/ba/AlfredDeakin.jpeg",
-    party: "Protectionist"
-  },
-  {
-    name: "Andrew Fisher",
-    start: "13/November/1908",
-    end: "2/June/1909",
-    image:
-        "https://upload.wikimedia.org/wikipedia/commons/b/be/Andrew_Fisher_1908.jpg",
-    party: "Labour"
-  }
-  ,
-  {
-    name: "Alfred Deakin",
-    start: "2/June/1909",
-    end: "29/April/1910",
-    image:
-        "https://upload.wikimedia.org/wikipedia/commons/b/ba/AlfredDeakin.jpeg",
+      "https://upload.wikimedia.org/wikipedia/commons/4/47/Joseph_Cook_-_Crown_Studios_03.jpg",
     party: "Commonwealth Liberal"
   },
   {
     name: "Andrew Fisher",
-    start: "29/April/1910",
-    end: "24/June/1913",
+    start: "17/September/1914",
+    end: "27/October/1915",
     image:
-        "https://upload.wikimedia.org/wikipedia/commons/b/be/Andrew_Fisher_1908.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/7/74/AndrewFisher.jpg",
     party: "Labour"
+  },
+  {
+    name: "Billy Hughes",
+    start: "17/September/1914",
+    end: "14/November/1916",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/d/d7/Billy_Hughes_1915.jpg",
+    party: "Labour"
+  },
+
+  {
+    name: "Billy Hughes",
+    start: "14/November/1916",
+    end: "17/February/1917",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/d/d7/Billy_Hughes_1915.jpg",
+    party: "National Labor"
+  },
+
+  {
+    name: "Billy Hughes",
+    start: "17/February/1917",
+    end: "9/February/1923",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/d/d7/Billy_Hughes_1915.jpg",
+    party: "Nationalist"
+  },
+
+  {
+    name: "Stanley Bruce",
+    start: "9/February/1923",
+    end: "22/October/1929",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/c/c4/Stanley_Bruce_-_Stoneman.jpg",
+    party: "Nationalist (Coalition)"
+  },
+
+  {
+    name: "James Scullin",
+    start: "22/October/1929",
+    end: "6/January/1932",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/9/97/Portrait_of_the_Right_Hon._J._H._Scullin.png",
+    party: "Labour"
+  },
+
+  {
+    name: "Joseph Lyons",
+    start: "6/January/1932",
+    end: "7/April/1939",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/3/31/Joseph_Lyons.jpg",
+    party: "United Australia (Coalition post-1934)"
+  },
+
+  {
+    name: "Sir Earle Page",
+    start: "7/April/1939",
+    end: "26/April/1939",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Earle_Page.jpg/800px-Earle_Page.jpg",
+    party: "Country (Coalition)"
+  },
+
+  {
+    name: "Robert Menzies",
+    start: "26/April/1939",
+    end: "24/August/1941",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Robert_Menzies_in_1939.jpg/800px-Robert_Menzies_in_1939.jpg",
+    party: "United Australia (Coalition post-1940)"
+  },
+
+  {
+    name: "Arthur Fadden",
+    start: "24/August/1941",
+    end: "7/October/1941",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/FaddenPEO.jpg/800px-FaddenPEO.jpg",
+    party: "Country (Coalition)"
+  },
+
+  {
+    name: "John Curtin",
+    start: "7/October/1941",
+    end: "5/July/1945",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/JohnCurtin.jpg/800px-JohnCurtin.jpg",
+    party: "Labor"
+  },
+
+  {
+    name: "Frank Forde",
+    start: "6/July/1945",
+    end: "13/July/1945",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Frank_Forde_1945.jpg/800px-Frank_Forde_1945.jpg",
+    party: "Labor"
+  },
+
+  {
+    name: "Ben Chifley",
+    start: "13/July/1945",
+    end: "19/December/1949",
+    image: "https://upload.wikimedia.org/wikipedia/commons/c/cd/Benchifley.jpg",
+    party: "Labor"
+  },
+
+  {
+    name: "Sir Robert Menzies",
+    start: "19/December/1949",
+    end: "26/January/1966",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Portrait_Menzies_1950s.jpg/800px-Portrait_Menzies_1950s.jpg",
+    party: "Liberal (Coalition)"
+  },
+  {
+    name: "Harold Holt",
+    start: "26/January/1966",
+    end: "19/December/1967",
+    image: "https://upload.wikimedia.org/wikipedia/commons/5/58/Harold_Holt_1965_01.jpg",
+    party: "Liberal (Coalition)"
+  },
+
+  {
+    name: "John McEwen",
+    start: "19/December/1967",
+    end: "10/January/1968",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/Sir_John_McEwen.jpg/800px-Sir_John_McEwen.jpg",
+    party: "Country (Coalition)"
+  },
+
+  {
+    name: "John Gorton",
+    start: "10/January/1968",
+    end: "10/March/1971",
+    image: "https://upload.wikimedia.org/wikipedia/commons/d/d3/JohnGorton1968.jpg",
+    party: "Liberal (Coalition)"
+  },
+
+  {
+    name: "William McMahon",
+    start: "10/March/1971",
+    end: "5/December/1972",
+    image: "https://upload.wikimedia.org/wikipedia/commons/0/0b/McMahon_1971_%28cropped%29.jpg",
+    party: "Liberal (Coalition)"
+  },
+
+  {
+    name: "Gough Whitlam",
+    start: "5/December/1972",
+    end: "11/November/1975",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/Gough_Whitlam_-_ACF_-_crop.jpg/800px-Gough_Whitlam_-_ACF_-_crop.jpg",
+    party: "Labor"
+  },
+
+  {
+    name: "Malcolm Fraser",
+    start: "11/November/1975",
+    end: "11/March/1983",
+    image: "https://upload.wikimedia.org/wikipedia/commons/7/76/Malcolm_Fraser_1977_-_crop.jpg",
+    party: "Liberal (Coalition)"
+  },
+
+  {
+    name: "Bob Hawke",
+    start: "11/March/1983",
+    end: "20/December/1991",
+    image: "https://upload.wikimedia.org/wikipedia/commons/2/29/Bob_Hawke_1987_portrait_crop.jpg",
+    party: "Labor"
+  },
+
+  {
+    name: "Paul Keating",
+    start: "20/December/1991",
+    end: "11/March/1996",
+    image: "https://upload.wikimedia.org/wikipedia/commons/f/ff/Paul_Keating_1985.jpg",
+    party: "Labor"
+  },
+
+  {
+    name: "John Howard",
+    start: "11/March/1996",
+    end: "3/December/2007",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Image-Howard2003upr.JPG/800px-Image-Howard2003upr.JPG",
+    party: "Liberal (Coalition)"
+  },
+
+  {
+    name: "Kevin Rudd",
+    start: "3/December/2007",
+    end: "24/June/2010",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Kevin_Rudd_official_portrait.jpg/800px-Kevin_Rudd_official_portrait.jpg",
+    party: "Labor"
+  },
+
+  {
+    name: "Julia Gillard",
+    start: "24/June/2010",
+    end: "27/June/2013",
+    image: "https://upload.wikimedia.org/wikipedia/commons/c/c8/Julia_Gillard_2010.jpg",
+    party: "Labor"
+  },
+
+  {
+    name: "Kevin Rudd",
+    start: "27/June/2013",
+    end: "18/September/2013",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/The_Hon._Kevin_Rudd.jpg/800px-The_Hon._Kevin_Rudd.jpg",
+    party: "Labor"
+  },
+
+  {
+    name: "Tony Abbott",
+    start: "18/September/2013",
+    end: "15/September/2015",
+    image: "https://upload.wikimedia.org/wikipedia/commons/9/92/Prime_Minister_Tony_Abbott.jpg",
+    party: "Liberal (Coalition)"
+  },
+
+  {
+    name: "Malcolm Turnbull",
+    start: "15/September/2015",
+    end: "24/August/2018",
+    image: "https://upload.wikimedia.org/wikipedia/commons/d/d1/Malcolm_Turnbull_PEO_%28cropped%29.jpg",
+    party: "Liberal (Coalition)"
+  },
+
+  {
+    name: "Scott Morrison",
+    start: "24/August/2018",
+    end: "???",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Scott_Morrison_2014_crop.jpg/800px-Scott_Morrison_2014_crop.jpg",
+    party: "Liberal (Coalition)"
   }
+
 ];
